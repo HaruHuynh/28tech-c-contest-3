1f63a01300b7c215cdcdb8c404c50dbd89ab3c82
haruhuynh2208@gmail.com <haruhuynh2208@gmail.com>
2025-10-20 22:22:58 +0700

    Bài 2

---
+++ bai-02.c
@@ -0,0 +1,34 @@
+// Số nguyên dương N được gọi là số Sphenic nếu N được phân tích duy nhất dưới dạng tích của ba thừa số nguyên tố khác nhau.
+// Ví dụ : N = 30 là số Sphenic vì 30 = 2×3×5. N = 60 không phải số Sphenic vì 60 = 2×2×3×5. Cho số tự nhiên N, nhiệm vụ của bạn là kiểm tra xem N có phải số Sphenic hay không?
+#include <stdio.h>
+#include <math.h>
+int sphenic (long long so)  {
+    int count = 0;
+    for (long long i = 2; i <= sqrt(so); i++){
+        if (so % i == 0){
+            int bac = 0;
+            while (so % i == 0){
+                so /= i;
+                bac++;
+                if(bac > 1) return 0;
+            }
+            count++;
+            if(count > 3) return 0;
+        }
+    }
+    if(so != 1) count++;
+    return count == 3;
+}
+
+int main () {
+    long long n;
+    do {
+        scanf("%lld", &n);
+    }while (n < 2);
+
+    if(sphenic(n)){
+        printf("1");
+    }else{
+        printf("0");
+    }
+}
