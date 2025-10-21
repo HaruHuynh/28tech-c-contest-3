fa7eade186c56d51a8c41498cedd41b6733424f5
haruhuynh2208@gmail.com <haruhuynh2208@gmail.com>
2025-10-21 22:58:20 +0700

    Review (1->5)

--- fix_bai-02.c
+++ fix_bai-02.c
@@ -2,21 +2,21 @@
 #include <math.h>
 
 int nt(long long n) {
-    int cnt = 0;
-    for(int i = 2; i <= sqrt(n); i++){
-        if(n % i == 0){
-            int mu = 0;
-            while(n % i == 0){
-                ++mu;
-                n /= i;
+    int cnt = 0; // Khoi tao bien dem
+    for(int i = 2; i <= sqrt(n); i++){ // Duyet mang i xuat phat tu 2
+        if(n % i == 0){ // Neu nhu n chia het cho i
+            int mu = 0; // Khoi tao bien mu ban dau = 0
+            while(n % i == 0){ // Neu n tiep tuc chia het cho i
+                ++mu; // Tang bien mu len 1 don vi
+                n /= i; // Thuc hien n chia i
             }
-            if(mu >= 2) return 0;
-            ++cnt;
+            if(mu >= 2) return 0; // Neu mu = 2 hoac mu > 2 -> Loai
+            ++cnt; // Tang bien dem len 1 don vi
         }
     }
-    if(n > 1)
-        ++cnt;
-    if(cnt == 3)
+    if(n > 1) // Neu nhu con sot lai n 
+        ++cnt; // Tiep tuc tang bien dem
+    if(cnt == 3) // Neu bien dem = 3 -> DUNG, neu khong -> SAI
         return 1;
     else 
         return 0;
