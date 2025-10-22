a6a6e906e08c4750763e5c341ac054725f9ea580
haruhuynh2208@gmail.com <haruhuynh2208@gmail.com>
2025-10-22 11:57:07 +0700

    Review-0.2 (1->5)

--- fix_bai-05.c
+++ fix_bai-05.c
@@ -11,25 +11,25 @@ int tongcs(int n){
 }
 
 int smith(int n){
-    int tmp = n;
-    int sum1 = tongcs(n);
-    int sum2 = 0;
-    for(int i = 2; i <= sqrt(n); i++){
+    int tmp = n; //Cho so nhap vao n ban dau vao bien tam
+    int sum1 = tongcs(n); //Goi ham tinh tong chu so cua n ban dau va them vao bien sum1
+    int sum2 = 0; //Khoi tao bien sum2 = 0 
+    for(int i = 2; i <= sqrt(n); i++){ //i khoi tao = 2 trong vong lap for de tim thua so 
         if(n % i == 0){
-            int d = tongcs(i);
+            int d = tongcs(i); //Goi ham tinh tong chu so de tinh tong cac thua so va thay vao bien d
             while(n % i == 0){
-                sum2+= d;
+                sum2+= d; //Thay bien d vao bien sum2 voi tat ca thua so
                 n /= i;
             } 
         }
     }
-    if(tmp == n)
-        return 0;
+    if(tmp == n) //Neu n khong phai la so Smith vi n la SNT
+        return 0; // Tra ve 0
     if(n > 1)
-        sum2 += tongcs(n);
-    if(sum1 == sum2)
+        sum2 += tongcs(n); //Neu con sot lai so n se cong don vao bien sum2
+    if(sum1 == sum2) //Neu tong sum1 = sum 2 => DUNG
         return 1;
-    else return 0;
+    else return 0; //Neu khong => SAI
 }
 
 int main(){
