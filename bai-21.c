902082a7c1382606f954597d0e28792df636b38f
haruhuynh2208@gmail.com <haruhuynh2208@gmail.com>
2025-10-24 22:16:36 +0700

    Bài 22

--- bai-21.c
+++ bai-21.c
@@ -1,6 +1,8 @@
 // Một số được coi là số đẹp khi nó đồng thời vừa chia hết cho một số nguyên tố và chia hết cho bình phương của số nguyên tố đó.
 // Hãy liệt kê các số đẹp như vậy trong đoạn giữa hai số nguyên dương a, b cho .
 // Gợi ý : Có 1 thừa số nguyên tố có số mũ >= 2 là thỏa mãn
+	// •	Chỉ cần tìm thấy một thừa số nguyên tố có số mũ ≥ 2 là trả về 1 (coi là “đẹp”).
+	// •	Nghĩa là kiểm tra điều kiện: “có ít nhất một nguyên tố chia hết bình phương”.
 
 #include <stdio.h>
 #include <math.h>
---
+++ bai-22.c
@@ -0,0 +1,41 @@
+// Một số được coi là số đẹp khi nếu nó chia hết cho một số nguyên tố nào đó thì cũng phải chia hết cho bình phương của số nguyên tố đó. Hãy liệt kê các số đẹp như vậy trong đoạn giữa hai số nguyên dương cho trước.
+// Gợi ý : Bài này thì số thỏa mãn phải đảm bảo tất cả các thừa số nguyên tố có số mũ >= 2
+	// •	Bắt buộc tất cả thừa số nguyên tố đều phải có mũ ≥ 2.
+	// •	Nếu có bất kỳ thừa số nào mũ = 1 thì loại.
+
+#include <stdio.h>
+#include <math.h>
+int snt(int n){
+    if(n < 2) return 0;
+    for(int i = 2; i <= sqrt(n); i++){
+        if(n % i == 0) return 0;
+    }
+    return 1;
+}
+
+int goodNum(int n){
+    for(int i = 2; i <= sqrt(n); i++){
+        if(n % i == 0){
+            int mu = 0;
+            while(n % i == 0){
+              mu++;
+              n /= i;
+            }
+            if(mu < 2)
+            return 0;
+        }
+    }
+    if(n > 1) return 0;
+    return 1;
+}
+
+
+int main() {
+    int a,b;
+    scanf("%d%d", &a,&b);
+    for(int i = a; i <= b; i++){
+        if(goodNum(i)) 
+            printf("%d ", i);
+    }
+    return 0;
+}
